05d7d5cdd01321c2a617fbbd889eb090c1fb73ee
tkdals1119 <asb1119@naver.com>
2017-12-14 14:07:26 +0900

    [추가] check_anipung

--- src/AniPung.java
+++ src/AniPung.java
@@ -14,8 +14,10 @@ public class AniPung {
 	}
 
 	private void calculate(int[][] anipung) { // 연속 3개 이상의 숫자가 동일 할 시 0으로 대체, 행 계산과 열 계산을 따로 함
-				
+		int[][] check_anipung = new int[5][5]; // 숫자를 바로 0으로 바꾸지 않고 가로, 세로 판별이 끝난 뒤 동시에 바꾸기 위해 해당 인덱스 위치를 저장하기 위한 배열
+
 		final_check = 0;
+		
 		for(int i=0; i<5; i++) // 행의 판별
 		{
 			count=0;
@@ -36,13 +38,13 @@ public class AniPung {
 							if(anipung[i][z] != 0) // 더 이상 판별할 숫자가 없다는 말은 세개 이상의 연속된 동일한 숫자가 없다는 뜻. 이 때는 동일한 숫자가 0밖에 없으므로 0끼리만 계산이 됨.
 								                   // 즉, 아직 판별할 숫자가 있다는 것은 0이 아닌 연속된 숫자들을 0으로 바꿀게 있다는 뜻이므로 이 때 final_check 변수를 1로 선언
 							{
-								anipung[i][z]=0;
+								check_anipung[i][z]=1; // 나중에 0으로 바꿔야할 인덱스 위치를 1로 표시해둠
 								final_check=1; // final_check가 0이면 더 이상 판별 할 숫자가 없음
 								               // final_check가 1이면 아직 판별 할 숫자 있음
 							}
 							else
 							{
-								anipung[i][z]=0;
+								check_anipung[i][z]=0;
 							}						
 						}
 					}
@@ -55,12 +57,12 @@ public class AniPung {
 						{
 							if(anipung[i][z] != 0)
 							{
-								anipung[i][z]=0;
+								check_anipung[i][z]=1;
 								final_check=1;
 							}
 							else
 							{
-								anipung[i][z]=0;
+								check_anipung[i][z]=0;
 							}								
 						}
 					}
@@ -68,7 +70,6 @@ public class AniPung {
 				}
 			}
 		}
-		
 		
 		for(int j=0; j<5; j++) // 열의 판별, 기본 로직은 행의 판별과 동일
 		{
@@ -91,12 +92,12 @@ public class AniPung {
 						{
 							if(anipung[z][j] != 0)
 							{
-								anipung[z][j]=0;
+								check_anipung[z][j]=1;
 								final_check=1;
 							}
 							else
 							{
-								anipung[z][j]=0;
+								check_anipung[z][j]=0;
 							}
 						}
 					}
@@ -109,12 +110,12 @@ public class AniPung {
 						{
 							if(anipung[z][j] != 0)
 							{
-								anipung[z][j]=0;
+								check_anipung[z][j]=1;
 								final_check=1;
 							}
 							else
 							{
-								anipung[z][j]=0;
+								check_anipung[z][j]=0;
 							}						
 						}
 					}
@@ -123,6 +124,20 @@ public class AniPung {
 			}
 		}	
 		
+		
+		// 1로 표시해둔 인덱스 위치를 anipung 에서 모두 0으로 바꿈
+		for(int x=0; x<5; x++)
+		{
+			for(int y=0; y<5; y++)
+			{
+				if(check_anipung[x][y] == 1) 
+				{
+					anipung[x][y] = 0;
+				}
+			}
+		}
+		
+
 		if(final_check == 0) // final_check가 함수 초기에 선언한 0 그대로 이므로, 더 이상 판별할 숫자가 없어 show 함수를 통해  최종 결과를 출력
 		{
 			show(anipung);
